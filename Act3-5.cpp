9d0cb07b4ea81c7ddda24451cdd5fe4f7430a4f2
e-liah <111042233+e-liah@users.noreply.github.com>
2023-02-25 11:09:10 +0800

    Update Act3-5.cpp

--- Act3-5.cpp
+++ Act3-5.cpp
@@ -22,15 +22,15 @@ void displayUppercase() {
   cin.get(name1, 100);
   cin.ignore();
 
-  for (int n = 0; n < strlen(name1); n++) {
+  for (int n = 0; n < strlen(name1); n++) { //to display all characters in lower case first
     name1[n] = tolower(name1[n]);
   }
 
-  for (int n = 0; n < strlen(name1); n++) {
+  for (int n = 0; n < strlen(name1); n++) { //to display the first letter of each word into uppercase
     if (n == 0) {
       name1[n] = toupper(name1[n]);
     }
-    else if (name1[n-1] == ' ') {
+    else if (name1[n-1] == ' ') { //finds the white space in the string and capitalizes the next character
       name1[n] = toupper(name1[n]);
     }
   }
