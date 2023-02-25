9f37fea730e12d7be9236431976c0d51c0a48429
e-liah <111042233+e-liah@users.noreply.github.com>
2023-02-25 10:56:26 +0800

    Update Act3-1.cpp

--- Act3-1.cpp
+++ Act3-1.cpp
@@ -28,15 +28,15 @@ void displayCompare() {
   cin.get(name2, 50);
   cin.ignore();
 
-  if (strcmp(name1, name2) == 0) {
+  if (strcmp(name1, name2) == 0) { //if both strings have the same ASCII value
     cout << "equal" << endl;
   }
 
-  else if (strcmp(name1, name2) > 0) {
+  else if (strcmp(name1, name2) > 0) { // if the first string has a greater ASCII value
     cout << "positive" << endl;
   }
 
-  else if (strcmp(name1, name2) < 0) {
+  else if (strcmp(name1, name2) < 0) { //if the second string has a greater ASCII value
     cout << "negative" << endl;
   }
 
